7306841852b2bf0ae732dc90eb6eee18f908e916
lestorrr <meiravalderama@gmail.com>
2023-11-14 22:35:40 +0800

    11

--- Interactingwiththepage.py
+++ Interactingwiththepage.py
@@ -11,24 +11,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
 
-option = Options()
-option.add_experimental_option("detach",True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-options=option)
+#chrome malala
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get('http://p1wwebd08/lmsv2/auth/login')
+#link mong malala
+driver.get("HTTP LINK MO LODS")
 
-def test_login():
-    path = "http://p1wwebd08/lmsv2/auth/login"
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get(path)
-    ADaccount = "JMOYA"
-    password = "Ciana052497!3"
-    time.sleep(5)
-    AD=driver.find_element(By.ID,'employee')
-    AD.clear()
-    time.sleep(2)
-    AD.send_keys(ADaccount)
-    User=driver.find_
+
+#<input type="text" name="passwd" id="passwd-id" />    👈 ETO LALAGAY EH AYAW PADEN HAHAHA
+
+element = driver.find_element(By.ID, "passwd-id")
+element = driver.find_element(By.NAME, "passwd")
+element = driver.find_element(By.XPATH, "//input[@id='passwd-id']")
+element = driver.find_element(By.CSS_SELECTOR, "input#passwd-id")
+
