e90e64b37a1dadec860683d5ca31ff954c514b76
lestorrr <meiravalderama@gmail.com>
2023-11-21 15:01:48 +0800

    passed

---
+++ REGULAR USER/Course creation module.py
@@ -0,0 +1,28 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+#LINK NA I AUTOMATE AT TEST
+driver = webdriver.Chrome()
+driver.get("http://p1wwebd08/lmsv2/auth/login")
+driver.maximize_window()
+#ADMIN USER AT PASSWORD
+try:
+    driver.find_element(By.NAME, "employee_number").click()
+    driver.find_element(By.ID, "sfemployeenumber").send_keys("120313")
+    password_field = driver.find_element(By.ID, "sfpassword")
+    password_field.send_keys("120313071069")
+    driver.find_element(By.XPATH, "//*[@id='frmLMSAccount']/form/button").click()
+    time.sleep(1)
+
+#HOME KABANATAE
+    driver.execute_script("window.open('http://p1wwebd08/lmsv2/my-curriculum/index');")
+    submit_button = WebDriverWait(driver, 10).until
+    driver.find_element(By.XPATH, "xpath=//h3[@id='ui-id-4']").click()
+    time.sleep(1)
+
+finally:
+    time.sleep(500)
