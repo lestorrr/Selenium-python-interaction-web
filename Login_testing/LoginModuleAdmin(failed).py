68f9c7fc92d93396a79976da64b5a6e28f21a4fe
lestorrr <meiravalderama@gmail.com>
2023-11-22 16:05:59 +0800

    passed

---
+++ Login_testing/LoginModuleAdmin(failed).py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+#LINK NA I AUTOMATE AT TEST
+driver = webdriver.Chrome()
+driver.get("http://p1wwebd08/lmsv2/auth/login")
+driver.maximize_window()
+element = WebDriverWait(driver, 5).until(
+EC.presence_of_element_located((By.ID, "active_directory"))
+)
+element.click()
+#ADMIN USER AT PASSWORD
+try:
+    username_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "ademployee"))
+    )
+    password_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "password"))
+    )
+    username_field.send_keys("JMOYa")
+    password_field.send_keys("Ciana052497!0")
+
+    submit_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
+    )
+    submit_button.click()
+finally:
+    time.sleep(100)
