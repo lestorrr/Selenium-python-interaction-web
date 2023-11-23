a651ab30bf3d2345ccbd5ecd109180da65481e69
lestorrr <meiravalderama@gmail.com>
2023-11-23 16:09:20 +0800

    .

--- Test_Course101/Course_approval_flow_(Approve).py
+++ Test_Course101/Course_approval_flow_(Approve).py
@@ -74,28 +74,8 @@ try:
 
     time.sleep(1)
 
-    driver.get("http://p1wwebd08/lmsv2/auth/login")
-    driver.maximize_window()
-    element = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.ID, "active_directory"))
-    )
-    element.click()
-#ADMIN USER AT PASSWORD
-    username_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "ademployee"))
-    )
-    password_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "password"))
-    )
-    username_field.send_keys("JMOYA")
-    password_field.send_keys("Ciana052497!3")
+    driver.get("http://p1wwebd08/lmsv2/course/index/readonly")
+    element = WebDriverWait(driver, 5).until()
 
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
-    )
-    submit_button.click()
-    time.sleep(2)
-
-    driver.get("http://p1wwebd08/lmsv2/course/home")
 finally:
     time.sleep(100)
